f1b03183a92c2b1aa1e358124203787c5b2e4920
Brett Richter <nycbjr@gmail.com>
2014-07-23 12:27:17 -0400

    support for cmbroms

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/mondrianwifi/full_mondrianwifi.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common CMB stuff.
+$(call inherit-product, vendor/cmb/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_DEVICE := mondrianwifi
-PRODUCT_NAME := cm_mondrianwifi
+PRODUCT_NAME := cmb_mondrianwifi
